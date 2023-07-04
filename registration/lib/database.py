1760679eb516c68d250098385b85ab8328f9d9c3
Nikita Feldbush <nikita@feldbush.su>
2023-07-04 17:08:08 +0300

    db

--- registration/lib/database.py
+++ registration/lib/database.py
@@ -8,25 +8,8 @@ import pandas as pd
 
 class Connection:
     def __init__(self):
-        self.connection = sqlite3.connect("tic_tac_toe.db")
+        self.connection = sqlite3.connect("prod.db")
         self.cursor = self.connection.cursor()
-    def add_game_to_db(self, winner, moves_amount, position):
-        query=f'''INSERT INTO games(winner,moves_amount,position) VALUES ('{winner}',{moves_amount},'{position}')'''
-        self.cursor.execute(query)
-        self.connection.commit()
-    def game_instance(self):
-        try:
-            query='''SELECT * FROM games WHERE id=(SELECT max(id) FROM games);'''
-            self.cursor.execute(query)
-            df = pd.DataFrame(self.cursor.fetchall())
-            query=f'''INSERT INTO game_instance(id) VALUES ('{df[0][0]}')'''
-        except:
-            pass
-    def add_move(self, position, simbol):
-        try:
-            query='''SELECT * FROM game_instance WHERE id=(SELECT max(id) FROM game_instance);'''
-            self.cursor.execute(query)
-            df = pd.DataFrame(self.cursor.fetchall())
-            query=f'''UPDATE game_instance SET '{position}'= {simbol} WHERE id = {df[0][0]}'''
-        except:
-            pass
+if __name__ == "__main__":
+    conn=Connection()
+    conn.cursor.execute('''CREATE TABLE IF NOT EXISTS users(id integer PRIMARY KEY, login TEXT, password TEXT)''')    
