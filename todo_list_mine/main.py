1cb24625d8eff28336e78c5432e8e874dfb2e553
nik010802 <fn11100@gmail.com>
2023-08-08 23:07:36 +0700

    f

---
+++ todo_list_mine/main.py
@@ -0,0 +1,67 @@
+# -*- coding:utf-8 -*-
+
+from fastapi import FastAPI, Request, Form, Cookie
+from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from typing import Union
+from lib import database
+import starlette.status as status
+import uvicorn
+# ! - проверить
+
+'''
+'''
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def start_page_get(request: Request):
+    return templates.TemplateResponse('login.html', {'form': 'register', 'error': '', 'login': '', 'request': request})
+
+
+@app.post("/login", response_class=HTMLResponse)
+async def login_post(request: Request, login: str = Form(), password: str = Form()):
+    conn = database.Connection()
+    if(conn.check_auth(login, password)):
+        response = RedirectResponse('/home', status_code=302)
+        response.set_cookie(key='login',value=login)
+        return response
+    else:
+        return templates.TemplateResponse('/login.html', context={'request': request, 'error': 'wrong'})
+
+
+@app.post("/", response_class=HTMLResponse)
+async def start_page_post(request: Request, login: str = Form(), password: str = Form()):
+    conn = database.Connection()
+    if(conn.check_auth(login, password)):
+        response = RedirectResponse('/home', status_code=302)
+        response.set_cookie(key='login',value='login')
+        return response
+    else:
+        return templates.TemplateResponse('/login.html', context={'request': request, 'error': 'wrong'})
+
+
+@app.get("/register", response_class=HTMLResponse)
+async def registration_get(request: Request):
+    return templates.TemplateResponse('/register.html', {'request': request})
+
+
+@app.post("/registr_success", response_class=HTMLResponse)
+async def registration_post(request: Request, login: str = Form(), password: str = Form()):
+    conn = database.Connection()
+    user = conn.create_user(login, password)
+    if(user == True):
+        return templates.TemplateResponse('/login.html', {'error': 'success', 'request': request})
+    else:
+        return templates.TemplateResponse('/register.html', context={'error': user, 'request': request})
+
+@app.get('/home',response_class=HTMLResponse)
+async def home_page_get(request: Request, login : str = Cookie(None)):
+    print(login)
+    return templates.TemplateResponse('/home_page.html',{'request':request})
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="37.140.192.188", port=8000, reload=True)
